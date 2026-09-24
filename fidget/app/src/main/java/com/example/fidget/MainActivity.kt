317e398c0430455e8c2ddc3545b45e57bb0ca9d1
Marko Kljakic <marko.kljakic1@gmail.com>
2026-09-23 18:28:24 -0600

    update shape class

--- fidget/app/src/main/java/com/example/fidget/MainActivity.kt
+++ fidget/app/src/main/java/com/example/fidget/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-abstract class Shape(x: Int, y: Int, val color: String = "$PUT_YOUR_CHOICE_OF_COLOR") {
+abstract class Shape(x: Int, y: Int, val color: String = "$orange") {
 
 }
 
